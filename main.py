b1276002d3fd133c2c53d59a9035997571184320
Konstantin <k.elfimow@gmail.com>
2023-01-31 12:58:22 +0300

    Initial commit!

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ def main():
     string: str = open('input.txt', encoding='utf-8', mode='r').read()
     result = lz77(string)
 
-    file_output = open('output.txt', encoding='utf-8', mode='w')
-    for counter, i in enumerate(range(len(result))):
-        file_output.write('{}. {}'.format(counter + 1, str(result[i])))
-        file_output.write('\n')
-    file_output.close()
+    with open('output.txt', encoding='utf-8', mode='w') as file_output:
+        for counter, i in enumerate(range(len(result))):
+            file_output.write('{}. {}'.format(counter + 1, str(result[i])))
+            file_output.write('\n')
 
 
 if __name__ == '__main__':
